5774878628540ee750786d062cba85e572adfefe
EminQasimov <eminqasimov@outlook.com>
2019-07-26 00:18:02 +0400

    ready responsivness

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,42 @@
-import 'bootstrap';
+import "bootstrap"
 
-import '../scss/index.scss';
+import "../scss/index.scss"
 
-$('#alert').click(() => {
-  alert('jQuery works!');
-});
+var open = false
 
+$(".menu-icon").click(function() {
+  open = !open
+  if (open) {
+    $(".menu-list").css({
+      display: "block",
+      position: "absolute",
+      background: "#7519b0",
+      right: "118px",
+      top: "31px"
+    })
+  } else {
+    $(".menu-list").css({
+      display: "none",
+      position: "absolute",
+      background: "#7519b0",
+      right: "118px",
+      top: "31px"
+    })
+  }
+})
+
+window.onresize = function() {
+  if (window.innerWidth > 1000) {
+    open = false
+    $(".menu-list").css({
+      display: "flex",
+      position: "static",
+      background: "transparent"
+    })
+  } else {
+    $(".menu-list").css({
+      display: "none"
+    })
+  }
+}
 // Your jQuery code
