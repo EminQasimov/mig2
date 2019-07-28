93e864fde907bac627712f13e62619510c45ac51
EminQasimov <eminqasimov@outlook.com>
2019-07-28 13:54:23 +0400

    menu

--- src/js/index.js
+++ src/js/index.js
@@ -3,5 +3,40 @@ import "../scss/welcome.scss"
 import "./todo"
 import "./azerbaijanis"
  
- 
+var open = false
 
+$(".menu-icon").click(function() {
+  open = !open
+  if (open) {
+    $(".menu-list").css({
+      display: "block",
+      position: "absolute",
+      background: "#7519b0",
+      right: "118px",
+      top: "31px"
+    })
+  } else {
+    $(".menu-list").css({
+      display: "none",
+      position: "absolute",
+      background: "#7519b0",
+      right: "118px",
+      top: "31px"
+    })
+  }
+})
+
+window.onresize = function() {
+  if (window.innerWidth > 1000) {
+    open = false
+    $(".menu-list").css({
+      display: "flex",
+      position: "static",
+      background: "transparent"
+    })
+  } else {
+    $(".menu-list").css({
+      display: "none"
+    })
+  }
+}
