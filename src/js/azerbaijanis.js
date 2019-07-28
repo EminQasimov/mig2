20b6c72084a44857d972ee163aa2caadbb3cf0b9
EminQasimov <eminqasimov@outlook.com>
2019-07-28 13:49:29 +0400

    not menu

--- src/js/azerbaijanis.js
+++ src/js/azerbaijanis.js
@@ -1,1 +1,1 @@
- 
+ import "../scss/azerbaijanis.scss";
--- src/js/index.js
+++ src/js/index.js
@@ -1,43 +1,7 @@
 import "bootstrap"
 import "../scss/welcome.scss"
-import "./todo.js"
-
-var open = false
-
-$(".menu-icon").click(function() {
-  open = !open
-  if (open) {
-    $(".menu-list").css({
-      display: "block",
-      position: "absolute",
-      background: "#7519b0",
-      right: "118px",
-      top: "31px"
-    })
-  } else {
-    $(".menu-list").css({
-      display: "none",
-      position: "absolute",
-      background: "#7519b0",
-      right: "118px",
-      top: "31px"
-    })
-  }
-})
-
-window.onresize = function() {
-  if (window.innerWidth > 1000) {
-    open = false
-    $(".menu-list").css({
-      display: "flex",
-      position: "static",
-      background: "transparent"
-    })
-  } else {
-    $(".menu-list").css({
-      display: "none"
-    })
-  }
-}
-
+import "./todo"
+import "./azerbaijanis"
+ 
+ 
 
